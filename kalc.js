dc579f1ed9a0a0d89eac77f54da9c00849426ae9
Avazbek Rakhimov <134701615+AvazRakhimov@users.noreply.github.com>
2023-12-17 21:49:23 +0500

    kalc.js

--- kalc.js
+++ kalc.js
@@ -4,7 +4,7 @@ const readline = require('readline'); // This one is for prompt
 
 //Welcome
 console.log("*************************************************")
-console.log("*****  Welcome to Kalculate! *****")
+console.log("************  Welcome to Kalculate! *************")
 
 // Code itself:
 
