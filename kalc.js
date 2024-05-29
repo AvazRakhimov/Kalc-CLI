76fb8a3a5671458d034dd7242ea1af6d3ed7ceea
Avaz Rakhimov <134701615+AvazRakhimov@users.noreply.github.com>
2024-05-29 17:10:17 +0500

    Update
    This update addresses to make code more clean.

--- kalc.js
+++ kalc.js
@@ -1,57 +1,28 @@
-// Modules (NodeJS):
 const readline = require('readline');
 
-// Welcome
 console.log("*************************************************")
 console.log("************* Welcome to Kalculate! *************")
 
-// This is for entering numbers
 const q = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-
-/* This will not work if write like (q1, q2, q3)!
-const q1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-
-const q2 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-*/
-
-
-// This type also does not work!
-
-// let n1 = new Number()
-// let n2 = new Number()
-
 let n1, n2
 
-// Main part
 q.question("****  Enter first number: ", (a0) => {
     if (a0 === "") {
         console.log("********  You didn't entered anything!  ********")
         q.close()
     } else {
-        //Input (convert to Number)
         n1 = new Number(a0)
-
         q.question("****  Enter second number: ", (a1) => {
             if (a1 === "") {
                 console.log("* You didn't enter anything for the second part *")
                 q.close()
             } else {
-                //Input (convert to Number)
                 n2 = new Number(a1)
-
                 q.question("Choose one of these ('+', '-', '*', '/', '%'): ", (a2) => {
-                    // *try* for catching errors
                     try {
                         switch (a2){
                         case ("+"):
